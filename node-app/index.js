c68f67bcc21f12074822faecca252fa438156e0a
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-10 16:45:41 +0530

    static-pages-commit

--- node-app/index.js
+++ node-app/index.js
@@ -1,8 +1,19 @@
 const express = require("express")
 var app = express()
+const path=require('path')
+
+//to configure static resource
+//static resources are resources that need to be sent as it is 
+app.use(express.static(path.join(__dirname, 'public/scripts')))
+app.use(express.static(path.join(__dirname, 'public/styles')))
+app.use(express.static(path.join(__dirname, 'public/pages')))
 
 app.get("/",function(request,response){
     response.send("Node APP is working and cool")
+})
+
+app.get("/home",function(request,response){
+    response.sendFile(path.join(__dirname,'public/pages/index.html'))
 })
 
 app.listen("8000",function(){
