e5884c004288286052f82e9e88778d53af8d349a
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-10 15:10:13 +0530

    require-commit

--- node-app/index.js
+++ node-app/index.js
@@ -1,1 +1,14 @@
-console.log("First node app")
+const fs = require("fs")
+
+
+fs.readFile("package1.json","utf-8",function(err,data){
+   if(err)
+      console.log("unable to read file")
+   else
+      console.log(data)
+})
+
+console.log("After attempting to read file")
+
+console.log("Still running")
+
