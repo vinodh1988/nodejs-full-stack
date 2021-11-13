25304d72777b098ffd5bcba02680cc92a7d6e0d4
vinodh1988 <k.c.vinodh@hotmail.com>
2021-11-13 15:20:26 +0530

    body-parser-commit

--- node-app/index.js
+++ node-app/index.js
@@ -1,19 +1,29 @@
 const express = require("express")
+const parser=require("body-parser")
 var app = express()
 const path=require('path')
 require('./app2')
 
+app.use(parser.urlencoded())
 //to configure static resource
 //static resources are resources that need to be sent as it is 
 app.use(express.static(path.join(__dirname, 'public/scripts')))
 app.use(express.static(path.join(__dirname, 'public/styles')))
-app.use(express.static(path.join(__dirname, 'public/pages')))
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use(express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+//app.use(express.static(path.join(__dirname, 'public/pages')))
 
 app.get("/",function(request,response){
     response.send("Node APP is working and cool")
 })
 
 app.get("/home",function(request,response){
+    response.sendFile(path.join(__dirname,'public/pages/home.html'))
+})
+
+app.post("/home",function(request,response){
+    console.log(request.body)
     response.sendFile(path.join(__dirname,'public/pages/home.html'))
 })
 
